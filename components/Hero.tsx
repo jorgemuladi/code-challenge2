8673c93b43ff4c17018285174d54e7f37562e532
Jorge Muladi <jorgeosattimuladi@gmail.com>
2024-10-16 18:24:45 +0700

    feat: added camp and guide section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,16 +4,16 @@ import { Button } from "./Button";
 const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      <div className="hero-map" />
+      <div className="hero-map overflow-hidden" />
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
           alt="camp"
           width={50}
           height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[5px]"
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of
           seeing the incorruptable beauty of nature. We can help you on an
